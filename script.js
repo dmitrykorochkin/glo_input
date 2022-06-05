d23120325b86d6feb480af6718ccd6641f30874c
Dmitry Korochkin <abramslost@gmail.com>
2022-06-05 10:07:19 +0300

    Выполнено задание

---
+++ script.js
@@ -0,0 +1,32 @@
+//Повесить на кнопку обработчик события click и реализовать такой функционал:
+
+const input = document.getElementById('text');  // input
+const btnTop = document.querySelector('#btn'); // вверхняя кнопка
+const square = document.querySelector('#square');  // square квадрат
+const btnBottom = document.querySelector('#e_btn');  // нижняя кнопка
+const inputRange = document.getElementById('range'); // ползунок
+const span = document.getElementById('range-span'); // значение ползунка
+const circle = document.getElementById('circle'); // круг 
+
+btnBottom.style.display = 'none';
+
+
+const logger = function (event){
+    square.style.backgroundColor = input.value;
+     
+};
+
+const range = function (e) {
+    span.textContent = e.target.value;
+    circle.style.width = span.textContent + '%';
+    circle.style.height = span.textContent + '%';
+};
+
+
+
+btn.addEventListener('click', logger);
+inputRange.addEventListener(input, logger);
+inputRange.addEventListener('change', range);
+
+
+
